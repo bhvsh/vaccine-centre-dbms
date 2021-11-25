baa207da7afdb9f69fe2f2bf6d7833f69f66a589
bhvsh <75534159+bhvsh@users.noreply.github.com>
2021-11-25 18:47:43 +0530

    Small changes on database design

--- query/V_viewMedicalRecord.sql
+++ query/V_viewMedicalRecord.sql
@@ -1,3 +1,4 @@
-CREATE VIEW V_viewMedicalRecord AS
-SELECT ssn,name,gender,address,phone,tookDose1,tookDose2,nextDue
-FROM People;
+CREATE VIEW W_viewVacRecord AS
+SELECT buid,bssn,bname,bgender,baddress,bphone,vname,vbrand,tookDose1,tookDose2,nextDue
+FROM Beneficiary B,Vaccine V,VaccineRecord VR
+WHERE B.buid=VR.buid AND VR.vid=V.vid;
