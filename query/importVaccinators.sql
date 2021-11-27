474cb3fb679b390979dc4a5947eae16fba7b5d45
bhvsh <75534159+bhvsh@users.noreply.github.com>
2021-11-28 01:29:58 +0530

    Added CSV import queries for Beneficiary and Vaccinators table

---
+++ query/importVaccinators.sql
@@ -0,0 +1,11 @@
+-- query that will import all the rows into the 'Vaccinators' table using .csv file
+
+BULK INSERT vDB_sch.Vaccinators
+FROM 'Vaccinator.csv'
+WITH
+(
+    FIRSTROW = 2, -- as 1st one is header
+    FIELDTERMINATOR = ',',  --CSV field delimiter
+    ROWTERMINATOR = '\n',   --Use to shift the control to next row
+    TABLOCK
+);
